f8f68c9c074ae68195881b973ab290148698c654
IAFEnvoy <tanksheng545@126.com>
2025-07-24 17:01:52 +0800

    add item throw compat

--- common/src/main/java/com/iafenvoy/sow/item/SowSwordItem.java
+++ common/src/main/java/com/iafenvoy/sow/item/SowSwordItem.java
@@ -1,13 +1,13 @@
 package com.iafenvoy.sow.item;
 
-import com.iafenvoy.neptune.object.item.ThrowableWeaponItem;
 import com.iafenvoy.sow.registry.SowItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
 import java.util.function.Function;
 
-public class SowSwordItem extends ThrowableWeaponItem implements SowWeapon {
+public class SowSwordItem extends SwordItem implements SowWeapon {
     public SowSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Function<Settings, Settings> settings) {
-        super(toolMaterial, attackDamage, attackSpeed, settings.apply(new Settings().arch$tab(SowItemGroups.WEAPONS)), new ThrowSettings());
+        super(toolMaterial, attackDamage, attackSpeed, settings.apply(new Settings().arch$tab(SowItemGroups.WEAPONS)));
     }
 }
